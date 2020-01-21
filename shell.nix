b5c35ff5b2e7104e94784343350f5818d27c3bbc
Dave King <dave@davbo.uk>
2020-01-21 09:07:30 +0000

    Support wasm build target

--- shell.nix
+++ shell.nix
@@ -1,14 +1,23 @@
-with import <nixpkgs> {};
+with import <nixpkgs> {
+  overlays = map (uri: import (fetchTarball uri)) [
+    https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz
+  ];
+};
 
 stdenv.mkDerivation {
   name = "rust-env";
   nativeBuildInputs = [
-    rustc cargo
+    nodejs
+    wasm-pack
   ];
   buildInputs = [
     # Example Run-time Additional Dependencies
+    (latest.rustChannels.nightly.rust.override {
+      targets = ["wasm32-unknown-unknown"];
+    })
   ];
 
   # Set Environment Variables
   RUST_BACKTRACE = 1;
+  NO_HEADLESS = 1;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,23 @@
 mod yara;
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+// Import the `window.alert` function from the Web.
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn parse() {
+    let input = br#"
+rule rule_name
+{
+    meta:
+        description = "This is just an example"
+}
+        "#;
+    let result = yara::rule().parse(input);
+    alert(&format!("Parsed: {:#?}!", result));
+}
