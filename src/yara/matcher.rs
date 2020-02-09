d7fb6d6d59f5caaef8c5ded4d0f2b55c7bbe3d81
Dave King <dave@davbo.uk>
2020-02-09 18:54:31 +0000

    Use strings accessor method

--- src/yara/matcher.rs
+++ src/yara/matcher.rs
@@ -2,17 +2,18 @@ use crate::yara::parser::{YaraIdentifier, YaraRule, YaraSections, YaraStrings};
 use std::collections::HashMap;
 
 impl YaraRule {
-  fn strings(&self) -> &HashMap<YaraIdentifier, YaraStrings> {
-      for section in &self.sections {
-          match section {
-              YaraSections::Strings(strings) => return strings,
-              _ => continue
-          }
-      };
-      panic!()
-  }
+    fn strings(&self) -> &HashMap<YaraIdentifier, YaraStrings> {
+        for section in &self.sections {
+            match section {
+                YaraSections::Strings(strings) => return strings,
+                _ => continue
+            }
+        };
+        panic!()
+    }
 
-  fn check_strings(&self, strings: &HashMap<YaraIdentifier, YaraStrings>, payload: &[u8]) -> bool {
+    fn check_strings(&self, payload: &[u8]) -> bool {
+        let strings = self.strings();
         for (_, strs) in strings {
             if match strs {
                 YaraStrings::Str(s) => payload.windows(s.len()).position(|win| win == s.as_bytes()).is_some(),
@@ -23,13 +24,7 @@ impl YaraRule {
     }
 
     pub fn matches(&self, payload: &[u8]) -> bool {
-        for section in &self.sections {
-            if match section {
-                YaraSections::Strings(strings) => self.check_strings(strings, payload),
-                _ => false
-            } { return true }
-        }
-        false
+        self.check_strings(payload)
     }
 }
 
