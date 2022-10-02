d8f47e72d9755a0ba57c26e992514814b17c9209
Dave King <dave@davbo.uk>
2022-10-02 17:50:17 +0300

    Restructure without wasm-bindgen
    To keep demos working I'll need to create a separate package which uses
    yara-rs that way makes more sense anyway.

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,7 @@
 mod yara;
-extern crate wasm_bindgen;
 
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
-
-
-#[wasm_bindgen]
 pub fn yara_match(rules: &str, payload: &[u8]) -> bool {
     let rule = yara::parser::rule().parse(rules.as_bytes()).unwrap();
     let result = rule.matches(payload);
-    if result {
-        log(&format!("Match against rule: {}", rule.name));
-    }
     result
-}
-
-#[wasm_bindgen]
-pub fn parse() {
-    let input = br#"
-rule test
-{
-    meta:
-        description = "This is just an example"
-    strings:
-        $a = { 66 [0-9] 7? }
-    condition:
-        $a
-}
-        "#;
-    let result = yara::parser::rule().parse(input);
-    log(&format!("Parsed: {:#?}!", result));
-    log(&format!(
-        "Matches: {:#?}!",
-        result.unwrap().matches(b"foo bar baz")
-    ));
 }
